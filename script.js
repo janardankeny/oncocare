d7937f28d22a6175850972b04517b7a362bff052
Janardan Keny <113849254+janardankeny@users.noreply.github.com>
2024-04-06 22:03:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; 
-const API_KEY = "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"; 
+const API_KEY = ""; 
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
